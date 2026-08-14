22e7e53a6f17b290f70158408a0fb3794defc2e0
a <a@a.com>
2026-08-14 13:57:47 +0800

    custom_file默认留空

--- cbuild_custom.cmake
+++
@@ -1,5 +0,0 @@
-# User-maintained extension loaded by cbuild_toolchain.cmake before vcpkg.
-#
-# Put project-specific toolchain settings here, for example:
-# set(VCPKG_OVERLAY_TRIPLETS "${CMAKE_CURRENT_LIST_DIR}/triplets")
-# set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -DMY_PROJECT_OPTION=1")
